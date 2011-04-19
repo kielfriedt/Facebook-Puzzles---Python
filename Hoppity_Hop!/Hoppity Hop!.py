622196f22150528ede14c8b3d7a253621cc6fba2
Kiel <kiel.friedt@gmail.com>
2011-04-18 17:38:19 -0700

    added comments to Hoppity hop 	modified:   Hoppity_Hop!/Hoppity Hop!.py

--- Hoppity_Hop!/Hoppity Hop!.py
+++ Hoppity_Hop!/Hoppity Hop!.py
@@ -1,13 +1,14 @@
 import sys;
 
+#Checks arguments
 if (len(sys.argv) > 2):
 	sys.exit("Error: see arguments");
 arg = sys.argv[1];
-try:
-	fp = open(arg, 'r');
+try: #Start of try
+	fp = open(arg, 'r'); #Opens input file
 	try:
 		value = int(fp.readline());
-		for x in xrange(1,value+1):
+		for x in xrange(1,value+1): #Loops through value from 1 to value
 			if(x % 5 == 0 and x % 3 == 0):
 				print("Hop\n");
 			elif(x % 5 == 0):
@@ -15,8 +16,8 @@ try:
 			elif(x %3 == 0):
 				print("Hoppity\n");
 	finally:                                    
-		fp.close();
-except IOError:
+		fp.close(); #Close file pointer
+except IOError: #If filename in argument doesn't exist it errors out
       sys.exit("cannot open " + arg);
 	
 
